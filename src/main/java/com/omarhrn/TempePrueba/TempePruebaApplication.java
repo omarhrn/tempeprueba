c12dac6521deebf6e39dc605e83581df2831dda8
omarhrn <omarr1354@gmail.com>
2021-03-05 23:44:00 +0100

    Proyecto inicial Tempe

---
+++ src/main/java/com/omarhrn/TempePrueba/TempePruebaApplication.java
@@ -0,0 +1,13 @@
+package com.omarhrn.TempePrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempePruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempePruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omarhrn/TempePrueba/TempePruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarhrn.TempePrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempePruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
